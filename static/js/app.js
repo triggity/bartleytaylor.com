d54adf78558febc2f47ea0e36b3c652536cdc222
Michael Truong <truongmt@gmail.com>
2013-03-05 07:29:16 -0800

    status checkin, attempting parrallax scroll

--- static/js/app.js
+++ static/js/app.js
@@ -22,11 +22,11 @@ $(document).ready(function() {
   var artTitleFontSize = parseInt($artTitle.css('font-size'));
   var $nav = $('.nav');
   var windowScroll;
-  console.log(" post section 1");
+
 
   // Apply Fittext to article titles to make it scale responsively in a smooth fashion
   $artTitle.fitText(1, { minFontSize: '34px' });
-  console.log(" post section 2");
+
 
 
   // Identify if visitor has a large enough viewport for parallaxing title
@@ -37,7 +37,7 @@ $(document).ready(function() {
       return true;
     }
   }
-  console.log(" post section 3");
+
 
   // If large viewport and not mobile, parallax the title
   if(!isMobile) {
@@ -47,7 +47,7 @@ $(document).ready(function() {
       }
     });
   }
-  console.log(" post section 4");
+
 
   // Window gets large enough, need to recalc all parallaxing title values
   $(window).resize(function() {
@@ -55,7 +55,7 @@ $(document).ready(function() {
       slidingTitle();
     }
   });
-  console.log(" post section 5");
+
 
   // Functional parallaxing calculations
   function slidingTitle() {
@@ -65,20 +65,23 @@ $(document).ready(function() {
     
     //Slow scroll of .art-header-inner scroll and fade it out
     $artHeaderInner.css({
-      'margin-top' : -(windowScroll/3)+"px",
+      'margin-top' : (windowScroll/3)+"px",
       'opacity' : 1-(windowScroll/550)
+     
     });
 
+     console.log('inner: ' + windowScroll/3);
     //Slowly parallax the background of .art-header
     $artHeader.css({
-      'background-position' : 'center ' + (-windowScroll/8)+"px"
+      'background-position' : 'center ' + (windowScroll/8)+"px"
+      
     });
-
+    console.log('header: ' + windowScroll/8);
     //Fade the .nav out
     $nav.css({
       'opacity' : 1-(windowScroll/400)
     });
-  console.log(" post section 6");  
+
 
   }
 
