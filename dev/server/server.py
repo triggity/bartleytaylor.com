9b7568f691ceec8262cff8aa755f3abd72c4cb10
michael truong <truongmt@gmail.com>
2013-06-01 19:56:48 -0700

    able to send email through gmail

---
+++ dev/server/server.py
@@ -0,0 +1,42 @@
+import smtplib
+from email.mime.text import MIMEText
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/email", methods=['POST'])
+def email():
+
+    from_email = "btsmtpserver@gmail.com"
+    password = "m420j~!@"
+    to = "truongmt@gmail.com"
+
+    # Create a text/plain message
+    message = "email from : {} \n name:{} \n text: {} ".format(
+        request.form['email'], request.form['name'], request.form['text'])
+    msg = MIMEText(message)
+    msg['Subject'] = "New Email From Your Website"
+    msg['From'] = from_email
+    msg['To'] = to 
+
+    try:
+        s = smtplib.SMTP('smtp.gmail.com:587')
+        s.starttls()  
+        s.login(from_email, password)  
+        s.sendmail(from_email, to, msg.as_string())
+        s.quit()
+        print "Successfully sent email"
+    except Exception as e:
+        print "Error: unable to send email: ", e
+
+    return "from flask"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
+
+
